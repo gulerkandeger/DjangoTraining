5834df57b2f8041e985b3f7d9f513c1fd4cca628
gulerkandeger <you@example.com>
2024-05-27 10:06:11 +0300

    feat: use ready template

--- first_django/settings.py
+++ first_django/settings.py
@@ -136,3 +136,21 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
